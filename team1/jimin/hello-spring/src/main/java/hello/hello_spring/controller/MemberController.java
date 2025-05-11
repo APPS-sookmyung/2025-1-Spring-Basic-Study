df207d3638374f9fe034802d0c09601901d64511
jimni3155 <jimni3155@naver.com>
2025-05-12 01:34:16 +0900

    feat:5주차 과제

--- team1/jimin/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ team1/jimin/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -18,6 +18,7 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+        system.out.println("memberService = " +memberService.getClass());
     }
 
     @GetMapping(value = "/members/new")
