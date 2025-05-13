0d10e12521ce93443e912e3cc26164137bc99c79
Bangjh05 <everangel05@naver.com>
2025-05-14 07:29:12 +0900

    Feat: 5주차 실습

--- team2/방지희/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ team2/방지희/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -3,7 +3,21 @@ import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
 import java.util.List;
 import java.util.Optional;
+@Transactional
 public class MemberService {
+    public Long join(Member member) {
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        }
+        finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
+    }
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     public Long join(Member member) {
@@ -17,6 +31,19 @@ public class MemberService {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
+
+    public List<Member> findMembers() {
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        }
+        finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
+    }
+}
 
     public List<Member> findMembers() {
         return memberRepository.findAll();
